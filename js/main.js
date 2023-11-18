c0625935f1e1527fe6f85551db286b9d6d4ef487
John Arocha <jparocha777@gmail.com>
2023-11-18 14:19:45 -0500

    added Player Turn in HTML and CSS

--- js/main.js
+++ js/main.js
@@ -21,9 +21,21 @@ const colors = {
 
 //   3.1) Store the 9 elements that represent the squares on the page.
 const options = [...document.querySelectorAll('#board > div')]
-console.log('options \n',options)
+// console.log('options \n',options)
+
 // 4) Upon loading the app should:
 //   4.1) Initialize the state variables:
+function init() {
+    turn = 1
+    winner = null
+    board = [
+        [0, 0, 0], //col 0
+        [0, 0, 0], //col 1
+        [0, 0, 0], //col 2
+    ]
+    render()
+}
+
 //     4.1.1) Initialize the board array to 9 nulls to represent empty squares. The 9 elements will "map" to each square, where index 0 maps to the top-left square and index 8 maps to the bottom-right square.
 //     4.1.2) Initialize whose turn it is to 1 (player 'X'). Player 'O' will be represented by -1.
 //     4.1.3) Initialize winner to null to represent that there is no winner or tie yet. Winner will hold the player value (1 or -1) if there's a winner. Winner will hold a 'T' if there's a tie. 
