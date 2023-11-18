91a7e8562ecef59b3304b31856f051eb0ac4b353
John Arocha <jparocha777@gmail.com>
2023-11-18 14:34:40 -0500

    Render Board Done" git push

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ const options = [...document.querySelectorAll('#board > div')]
 
 // 4) Upon loading the app should:
 //   4.1) Initialize the state variables:
-function init() {
+function start() {
     turn = 1
     winner = null
     board = [
@@ -33,9 +33,26 @@ function init() {
         [0, 0, 0], //col 1
         [0, 0, 0], //col 2
     ]
-    render()
+    // render()
 }
+start()
 
+function renderBoard() {
+    board.forEach((colArr, colIdx) => {
+        // console.log('colArr', colArr)
+        // console.log('colIdx', colIdx)
+        colArr.forEach((cellVal, rowIdx) => {
+            // console.log('cellVal', cellVal)
+            // console.log('rowIdx', rowIdx)
+            const cellId = `v${colIdx}r${rowIdx}`
+            // console.log('cellId', cellId)
+            const cellEl = document.getElementById(cellId)
+            // console.log('cellEl', cellEl)
+            cellEl.style.backgroundColor = colors[cellVal]
+        })
+    })
+}
+renderBoard()
 //     4.1.1) Initialize the board array to 9 nulls to represent empty squares. The 9 elements will "map" to each square, where index 0 maps to the top-left square and index 8 maps to the bottom-right square.
 //     4.1.2) Initialize whose turn it is to 1 (player 'X'). Player 'O' will be represented by -1.
 //     4.1.3) Initialize winner to null to represent that there is no winner or tie yet. Winner will hold the player value (1 or -1) if there's a winner. Winner will hold a 'T' if there's a tie. 
